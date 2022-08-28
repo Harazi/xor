a267fbe64ac392786b0e96083726bc4c83775eaa
husam <husam999h@gmail.com>
2022-08-28 12:12:46 +0300

    'keyPointer' became gloabal variable, so that it preserve state between function calls

--- encrypt.c
+++ encrypt.c
@@ -1,23 +1,18 @@
 #include <stdlib.h>
 #include "encrypt.h"
 
-char *encrypt(const char *message, void *key, size_t messageLength, size_t keyLength)
+size_t keyPointer = 0;
+
+char *encrypt(const char *message, const void *key, size_t messageLength, size_t keyLength)
 {
   char *encrypted = calloc(sizeof(char), messageLength + 1); // (+ 1) for null byte
-  size_t keyPointer = 0;
 
-  for (size_t i = 0; i < messageLength; i++)
+  for (size_t i = 0; i < messageLength; i++, keyPointer++)
   {
-    // will not encrypt the newline character
-    if (message[i] == '\n') {
-      encrypted[i] = message[i];
-      continue;
-    }
-
     if (keyPointer >= keyLength)
       keyPointer = 0;
 
-    encrypted[i] = message[i] ^ *(char*)(key + keyPointer++);
+    encrypted[i] = message[i] ^ *(char*)(key + keyPointer);
   }
 
   return encrypted;
