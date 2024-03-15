790ea20dd833303f14ff03659e3db9858d03514c
Husam Harazi <i@vimmer.dev>
2024-03-15 21:32:50 +0300

    Tab indentation

--- encrypt.c
+++ encrypt.c
@@ -5,15 +5,15 @@ size_t keyPointer = 0;
 
 char *encrypt(const char *message, const void *key, size_t messageLength, size_t keyLength)
 {
-  char *encrypted = calloc(sizeof(char), messageLength + 1); // (+ 1) for null byte
+	char *encrypted = calloc(sizeof(char), messageLength + 1); // (+ 1) for null byte
 
-  for (size_t i = 0; i < messageLength; i++, keyPointer++)
-  {
-    if (keyPointer >= keyLength)
-      keyPointer = 0;
+	for (size_t i = 0; i < messageLength; i++, keyPointer++)
+	{
+		if (keyPointer >= keyLength)
+			keyPointer = 0;
 
-    encrypted[i] = message[i] ^ *(char*)(key + keyPointer);
-  }
+		encrypted[i] = message[i] ^ *(char*)(key + keyPointer);
+	}
 
-  return encrypted;
+	return encrypted;
 }
--- xor.c
+++ xor.c
@@ -8,49 +8,49 @@
 
 void error(char *msg)
 {
-  fprintf(stderr, "%s: %s\n", msg, strerror(errno));
-  exit(EXIT_FAILURE);
+	fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+	exit(EXIT_FAILURE);
 }
 
 int main(int argc, char **argv)
 {
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s <encryption key>\n", argv[0]);
-    exit(EXIT_FAILURE);
-  }
-  if (argc > 2) {
-    fprintf(stderr, "Too many arguments!\n");
-    exit(EXIT_FAILURE);
-  }
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <encryption key>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	if (argc > 2) {
+		fprintf(stderr, "Too many arguments!\n");
+		exit(EXIT_FAILURE);
+	}
 
-  char *key = argv[1];
-  size_t keyLength = strlen(key);
+	char *key = argv[1];
+	size_t keyLength = strlen(key);
 
-  unsigned char hash[SHA512_DIGEST_LENGTH];
-  SHA512((u_char*)key, keyLength, hash);
+	unsigned char hash[SHA512_DIGEST_LENGTH];
+	SHA512((u_char*)key, keyLength, hash);
 
-  const size_t BUFFERSIZE = 4096;
-  char s[BUFFERSIZE];
-  ssize_t lineLen;
+	const size_t BUFFERSIZE = 4096;
+	char s[BUFFERSIZE];
+	ssize_t lineLen;
 
-  while ((lineLen = read(STDIN_FILENO, s, BUFFERSIZE)) != 0)
-  {
-    if (lineLen == -1)
-      error("Error while reading from stdin");
+	while ((lineLen = read(STDIN_FILENO, s, BUFFERSIZE)) != 0)
+	{
+		if (lineLen == -1)
+			error("Error while reading from stdin");
 
-    char *encryptedLine = encrypt(s, hash, lineLen, SHA512_DIGEST_LENGTH);
+		char *encryptedLine = encrypt(s, hash, lineLen, SHA512_DIGEST_LENGTH);
 
-    ssize_t written = write(STDOUT_FILENO, encryptedLine, lineLen);
+		ssize_t written = write(STDOUT_FILENO, encryptedLine, lineLen);
 
-    if (written == -1)
-      error("Error while calling write");
-    if (written != lineLen) {
-      fprintf(stderr, "write system call didn't write the exact amount of bytes:\nexpected %ld bytes\nwrote %ld bytes", lineLen, written);
-      exit(EXIT_FAILURE);
-    }
+		if (written == -1)
+			error("Error while calling write");
+		if (written != lineLen) {
+			fprintf(stderr, "write system call didn't write the exact amount of bytes:\nexpected %ld bytes\nwrote %ld bytes", lineLen, written);
+			exit(EXIT_FAILURE);
+		}
 
-    free(encryptedLine);
-  }
+		free(encryptedLine);
+	}
 
-  return 0;
+	return 0;
 }
