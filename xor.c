e9f27e649453163312b88636951d3c9bdd38ef30
Husam Harazi <i@vimmer.dev>
2024-03-16 16:22:27 +0300

    Cleanup & use stdio functions

--- xor.c
+++ xor.c
@@ -1,57 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <errno.h>
 #include "mince.h"
-
-void error(char *msg)
-{
-	fprintf(stderr, "%s: %s\n", msg, strerror(errno));
-	exit(EXIT_FAILURE);
-}
 
 int main(int argc, char **argv)
 {
-	if (argc < 2) {
+	if (argc != 2) {
 		fprintf(stderr, "Usage: %s <encryption key>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-	if (argc > 2) {
-		fprintf(stderr, "Too many arguments!\n");
-		exit(EXIT_FAILURE);
+		return EXIT_FAILURE;
 	}
 
 	int hashSize;
 	const unsigned char *hash = mince((unsigned char *)argv[1], &hashSize);
 
-	const size_t BUFFERSIZE = 4096;
-	char s[BUFFERSIZE];
-	ssize_t lineLen;
-	int keyPointer = 0;
+	char buf[BUFSIZ];
+	size_t read;
+	int hashPosition = 0;
 
-	while ((lineLen = read(STDIN_FILENO, s, BUFFERSIZE)) != 0)
-	{
-		if (lineLen == -1)
-			error("Error while reading from stdin");
+	while ((read = fread(buf, sizeof(char), BUFSIZ, stdin))) {
+		for (int i = 0; i < read; i++, hashPosition++) {
+			if (hashPosition >= hashSize)
+				hashPosition = 0;
 
-		for (int i = 0; i < lineLen; i++, keyPointer++)
-		{
-			if (keyPointer >= hashSize)
-				keyPointer = 0;
-
-			s[i] = s[i] ^ hash[keyPointer];
+			buf[i] = buf[i] ^ hash[hashPosition];
 		}
 
-		ssize_t written = write(STDOUT_FILENO, s, lineLen);
-
-		if (written == -1)
-			error("Error while calling write");
-		if (written != lineLen) {
-			fprintf(stderr, "write system call didn't write the exact amount of bytes:\nexpected %ld bytes\nwrote %ld bytes", lineLen, written);
-			exit(EXIT_FAILURE);
-		}
+		fwrite(buf, sizeof(char), read, stdout);
+	}
+	if (ferror(stdin)) {
+		perror("stdin");
+		return EXIT_FAILURE;
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
